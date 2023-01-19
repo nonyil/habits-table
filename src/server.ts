e38cb3412828468f8a09a54701d5df75f9f1c87f
nonyil <igbrendow@gmail.com>
2023-01-19 15:09:14 -0300

    feat: :fire: back-end configs
    ts-config, first-rote, prisma-config, cors-config

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+// Back-end API RESTful
+
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+  return habits
+})
+
+app.listen({
+  port: 3000,
+}).then(() => {
+  console.log("Server is running on port 3000");
+})

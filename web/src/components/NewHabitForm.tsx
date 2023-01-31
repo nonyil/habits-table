6415a004b1e6f01ac3be0a851964c014f2597e67
nonyil <igbrendow@gmail.com>
2023-01-31 15:47:34 -0300

    feat: obtendo dados do formulário

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,5 +1,6 @@
 import { Check } from "phosphor-react";
 import * as Checkbox from '@radix-ui/react-checkbox';
+import { FormEvent, useState } from "react";
 
 const availableWeekDays = [
   'Domingo',
@@ -12,8 +13,31 @@ const availableWeekDays = [
 ]
 
 export function NewHabitForm() {
+
+  const [title, setTitle] = useState('')
+  const [weekDays, setWeekDays] = useState<Number[]>([])
+
+  function createNewHabit(event: FormEvent) {
+    event.preventDefault()
+
+    console.log(title, weekDays);
+    
+  }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemovedOne = weekDays.filter((day => day !== weekDay))
+
+      setWeekDays(weekDaysWithRemovedOne)
+    } else {
+      const weekDaysWithAddOne = [...weekDays, weekDay]
+
+      setWeekDays(weekDaysWithAddOne)
+    }
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
@@ -24,6 +48,7 @@ export function NewHabitForm() {
         placeholder="Ex: Exercícios, dormir bem, etc..."
         className="p-4 rounded-large mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
+        onChange={event => setTitle(event.target.value)}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
@@ -32,9 +57,13 @@ export function NewHabitForm() {
 
       <div className="flex flex-col gap-2 mt-3">
 
-        {availableWeekDays.map((weekDay => {
+        {availableWeekDays.map(((weekDay, index) => {
           return (
-            <Checkbox.Root key={weekDay} className='flex items-center gap-3 group'>
+            <Checkbox.Root 
+              key={weekDay} 
+              className='flex items-center gap-3 group'
+              onCheckedChange={() => handleToggleWeekDay(index)}
+            >
             <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
               <Checkbox.Indicator>
                 <Check size={20} className='text-white' />

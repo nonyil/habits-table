dcb03b1cb0fc1fe25771b35d86040df0385bb2b1
nonyil <igbrendow@gmail.com>
2023-02-01 15:53:26 -0300

    feat: adiciona transitions e focus

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -3,18 +3,20 @@ interface ProgressBarProps {
 }
 
 export function ProgressBar(props: ProgressBarProps) {
-  const progressStyles = {
-    width: `${props.progress}%`
-  }
+  // const progressStyles = {
+  //   width: `${props.progress}%`
+  // }
   return (
     <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
-    <div
-      role="progressbar"
-      aria-label='Progresso de hábitos completados nesse dia'
-      aria-aria-valuenow={props.progress} 
-      className='h-3 rounded-xl bg-violet-600'
-      style={progressStyles}  
-    />
-  </div>
+      <div
+        role="progressbar"
+        aria-label='Progresso de hábitos completados nesse dia'
+        aria-valuenow={props.progress}
+        className='h-3 rounded-xl bg-violet-600 transition-all'
+        style={{
+          width: `${props.progress}%`
+        }}
+      />
+    </div>
   )
 }
